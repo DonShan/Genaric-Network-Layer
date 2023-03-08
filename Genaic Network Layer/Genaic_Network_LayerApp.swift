7d92714641117a6e0ddcdff953928dc1fab5f933
DonShan <msdjshan47@gmail.com>
2023-03-08 23:08:23 +0530

    Initial Commit

---
+++ Genaic Network Layer/Genaic_Network_LayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Genaic_Network_LayerApp.swift
+//  Genaic Network Layer
+//
+//  Created by Madushan Senavirathna on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct Genaic_Network_LayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
